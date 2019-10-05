8c5141c6274eb6b6f2f1a91f9ea4807d852a61de
MrNOOB <56033162+MrNOOB07@users.noreply.github.com>
2019-10-05 16:59:33 +0700

    Create sh b2.sh

---
+++ sh b2.sh
@@ -0,0 +1,34 @@
+black='\e[0;30m'
+blue='\e[0;34m'
+green='\e[0;32m'
+cyan='\e[0;36m'
+red='\e[0;31m'
+purple='\e[0;35m'
+brown='\e[0;33m'
+lightgray='\e[0;37m'
+darkgray='\e[1;30m'
+lightblue='\e[1;34m'
+lightgreen='\e[1;32m'
+lightcyan='\e[1;36m'
+lightred='\e[1;31m'
+lightpurple='\e[1;35m'
+yellow='\e[1;33m'
+white='\e[1;37m'
+nc='\e[0m'
+echo.
+echo"*🔰C1ONING CYBER ARMY*🔰------------------------------------------
+*Author : Mr.NOOB07*
+*My Team : C1ONING CYBER ARMY*
+*Contack Us : 0895xxxxx*
+------------------------------------------------
+*🔰SELAMAT MENCOBA*🔰"
+___________
+< WhatsApp >
+ ---------
+╲╲╭━━━━╮
+╭╮┃▆┈┈▆┃╭╮  root@Mr.NOOB07
+┃╰┫▽▽▽┣╯┃
+╰━┫△△△┣━╯
+╲╲┃┈┈┈┈┃
+╲╲┃┈┏┓┈┃
+▔▔╰━╯╰━╯
